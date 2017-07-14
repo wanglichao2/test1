097777993e17a2453e1d2cb4c6944afe3032b3cc
wanglichao2 <1724300211@qq.com>
2017-07-14 12:03:09 +0800

    准备修改

--- src/ddd/ss.java
+++ src/ddd/ss.java
@@ -4,10 +4,11 @@ public class ss {
   private int a;
   private int b;
   private int c;
-<<<<<<< HEAD
-=======
+
+
   public static void main(String[] args) {
      System.out.println("adfasdkf");
+     
 }
->>>>>>> fenzhitwo
+
 }
